91c0cc3b8fd09f35f0f5285bc979ca8facc6fc87
minikin <djminikin@gmail.com>
2023-01-03 23:22:43 +0100

    Fix CI, minor updates

--- example/base58_example.dart
+++ example/base58_example.dart
@@ -4,9 +4,9 @@ import 'package:base58/base58.dart';
 import 'package:base58/src/alphabet.dart';
 
 void main() {
-  final base58 = Base58(alphabet: Alphabet.btc());
+  final btcBase58 = Base58(alphabet: Alphabet.btc());
 
-  final encodedString = base58.encodeBase58(utf8.encode('bitcoin'));
+  final encodedString = btcBase58.encodeBase58(utf8.encode('bitcoin'));
 
   print(encodedString);
 }
